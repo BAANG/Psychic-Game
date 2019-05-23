22ea9ef0c5cdaa4ffabbe776b47814324ced46c2
BAANG <chcelestino@gmail.com>
2019-05-22 21:29:43 -0700

    Utilize .innerHtml to properly display number of guesses. All code working as intended

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,16 +7,19 @@ var guessMade = [];
 var alphabet = "abcdefghijklmnopqrstuvwxyz" // String to be utilized as array for alphabet selection and character validation
 var cpuRandom = alphabet[Math.floor(Math.random()*alphabet.length)] //cpu picks random letter in alphabet array
 
+
+
 document.onkeyup = function(input) { //function occurs on any key up
 
     var keyHistory = document.getElementById("guessHistory");
     var userWins = document.getElementById("wins");
     var userLosses = document.getElementById("losses");
-    var userGuess = document.getElementById("numGuess")
+    var userGuess = document.getElementById("numGuess").innerHTML = numGuess;
     var userKey = input.key.toLowerCase(); // Changes key input to lowercase for consistency
 
     console.log("This is the random letter: " + cpuRandom) // Console check for random letter generator
     console.log("Guesses left: " + numGuess)
+    userGuess.textcontent = numGuess;
 
     reset = function(){
         guessMade = [];
@@ -43,8 +46,8 @@ document.onkeyup = function(input) { //function occurs on any key up
 
         } else { //guess counter goes down if incorrect guess
             numGuess--;
-            userGuess.textcontent = numGuess;
-            if (numGuess === 0) { //after numGuess--, if numGuess=0, increase loss counter
+            // userGuess.textcontent = numGuess;
+            if (numGuess <= 0) { //after numGuess--, if numGuess=0, increase loss counter
                 losses++;
                 userLosses.textContent = losses;
                 reset();
@@ -54,10 +57,6 @@ document.onkeyup = function(input) { //function occurs on any key up
     } else { // if key press doesn't exist within alphabet
         alert("That is not a valid letter!");
     }
-
-   
-
-
 }
 
 // console.log("---userKey---")
